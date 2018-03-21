f1cb994e042194788965a89e9a12357680d80ba6
akinov <ayumoto10@gmail.com>
2018-03-21 11:13:15 +0900

    ログインを実装

--- office-bell/LoginViewController.swift
+++ office-bell/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  office-bell
 //
 //  Created by akinov on 2018/03/21.
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import Alamofire
 
 class LoginViewController: UIViewController {
-
+    // MARK: Propaties
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,30 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onClickLogin(_ sender: Any) {
+        Alamofire.request(
+            "http://192.168.11.7:3000/api/sign_in",
+            method: .post,
+            parameters: [
+                "email": emailField.text!,
+                "password": passwordField.text!
+            ])
+            .responseJSON { response in
+                if let json = response.result.value as? [String:[String]] {
+                    UserDefaults.standard.set(json["authentication_token"], forKey: "authentication_token")
+                    let storyboard: UIStoryboard = self.storyboard!
+                    let nextView = storyboard.instantiateViewController(withIdentifier: "mainViewController")
+                    self.present(nextView, animated: true, completion: nil)
+                }
+                else {
+                    let alertController = UIAlertController(title: "ログインに失敗しました", message: "正しいメールアドレス、パスワードをご入力ください", preferredStyle: UIAlertControllerStyle.alert)
+                    
+                    alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+        }
+    }
+    
 }
 
